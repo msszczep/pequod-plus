a3c4f94fb5c8827fdcf575f58793d895513deb7c
msszczep <mitchell@szcz.org>
2023-10-18 20:10:09 -0500

    Updating gen

--- src/clj/pequod_plus/gen.clj
+++ src/clj/pequod_plus/gen.clj
@@ -1,18 +1,17 @@
-(ns pequod-cljs.gen
-    (:require [pequod-cljs.ccs :as ccs]
-              [pequod-cljs.wcs :as wcs]))
+(ns pequod-plus.gen
+    (:require [pequod-plus.ccs :as ccs]
+              [pequod-plus.wcs :as wcs]))
 
 ; lein run -m pequod-cljs.gen NS-TO-USE
-
 
 (defn -main [& [ns-to-use]]
   (println (format "(ns pequod-cljs.%s)" ns-to-use))
   (println "")
   (println "(def ccs ")
-  (clojure.pprint/pprint (ccs/create-ccs-bulk 30000 10 100 100))
+  (clojure.pprint/pprint (ccs/create-ccs-bulk 300 10 10 10))
   (println ")")
   (println "(def wcs ")
-  (clojure.pprint/pprint (wcs/create-wcs-bulk 10000 10000 10000))
+  (clojure.pprint/pprint (wcs/create-wcs-bulk 100 100 100))
   (println ")")
 )
 
