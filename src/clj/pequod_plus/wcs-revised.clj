d5ef5bb98127cd736418a7840ff51c2f351c3564
msszczep <mitchell@szcz.org>
2024-02-08 21:25:02 -0600

    Good progress on WC data reorganization

--- src/clj/pequod_plus/wcs-revised.clj
+++ src/clj/pequod_plus/wcs-revised.clj
@@ -1,14 +1,13 @@
 (ns pequod-plus.wcs)
 
-; for however many inputs we have
-;  apportion 95 points among those inputs
-;  do some randomization such that it's within a suitable range
-;  the sum must be greater than 0.5, less than 1
+(defn create-wcs [num-of-worker-councils goods industry]
+  (->> goods
+       (mapv #(vec (concat (repeat (/ num-of-worker-councils (count goods))
+                                   {:industry industry 
+                                    :product %}))))
+       flatten))
 
-; range for 0.5 / number of inputs (length of flattened production-inputs)
-;  up to 1 / number of inputs
-
-(defn gen-wc [i product industry intermediate-input-types nature-types labor-types pollutants]
+(defn gen-wc [intermediate-input-types nature-types labor-types pollutants wc]
   (letfn [(get-random-subsets []
             (let [s (->> [1 1 1 1 2 2 3 3 4 4]
                          shuffle
@@ -28,80 +27,42 @@
             (->> types
                  shuffle
                  (take subset)
-                 sort))]
-    (let [[intermediate-input-subset 
+                 sort))
+          (populate-c-and-e [coefficients exponents]
+             (->> exponents
+                  (interleave coefficients)
+                  (partition 2)
+                  (map (fn [[a b]] (hash-map :coefficient a :exponent b)))))]
+    (let [[intermediate-inputs-subset 
            nature-subset 
            labor-subset 
            pollutant-subset] (get-random-subsets)
-          production-inputs-intermediate-input (determine-production-input intermediate-input-subset intermediate-input-types) 
+          production-inputs-intermediate-input (determine-production-input intermediate-inputs-subset intermediate-input-types)
           production-inputs-nature (determine-production-input nature-subset nature-types)
           production-inputs-labor (determine-production-input labor-subset labor-types)
           production-inputs-pollutants (determine-production-input pollutant-subset pollutants)
-          production-inputs-count (+ intermediate-inputs-suset nature-subset labor-subset pollutant-subset)
+          production-inputs-count (+ intermediate-inputs-subset nature-subset labor-subset pollutant-subset)
           intermediate-input-exponents (generate-exponents production-inputs-count production-inputs-intermediate-input)
           nature-exponents (generate-exponents production-inputs-count production-inputs-nature)
           labor-exponents (generate-exponents production-inputs-count production-inputs-labor)
           pollutant-exponents (generate-exponents production-inputs-count production-inputs-pollutants)
-          ; TODO interleave and populate exponents, inputs
           ]
-     (hash-map :id i
-               :output 0
-               :effort 0
-               :product product
-               :industry industry
-               :intermediate-inputs []
-               :cohort {:region 1}))))
+      (merge wc
+        (hash-map :output 0
+                  :effort 0
+                  :intermediate-inputs (populate-c-and-e production-inputs-intermediate-input intermediate-input-exponents)
+                  :nature (populate-c-and-e production-inputs-nature nature-exponents)
+                  :labor (populate-c-and-e production-inputs-labor labor-exponents)
+                  :pollutants (populate-c-and-e production-inputs-pollutants pollutant-exponents)
+                  )))))
 
-(defn continue-wc-setup [intermediate-input-types nature-types labor-types pollutants wc]
-  "Assumes wc is a map"
-  (letfn [(get-random-subsets []
-            (let [s (->> [1 1 1 1 2 2 3 3 4 4]
-                         shuffle
-                         (take (inc (rand-int 4)))
-                         frequencies)]
-              (->> [1 2 3 4]
-                   (map #(get s % 0))
-                   (mapv inc))))
-          (rand-range [start end]
-            (+ start (clojure.core/rand (- end start))))
-          (generate-exponents [n i inputs]
-            (->> #(rand-range (/ 0.75 n) (/ 0.85 n))
-                 repeatedly
-                 (take (count (nth inputs i)))
-                 vec))]
-    (let [[s1 s2 s3 s4] (get-random-subsets)
-          production-inputs (vector (into [] (sort (take s1 (shuffle intermediate-input-types))))
-                                    (into [] (sort (take s2 (shuffle nature-types))))
-                                    (into [] (sort (take s3 (shuffle labor-types))))
-                                    (into [] (sort (take s4 (shuffle pollutants)))))
-          production-inputs-count (->> production-inputs
-                                       flatten
-                                       count)
-          intermediate-input-exponents (generate-exponents production-inputs-count 0 production-inputs)
-          nature-exponents (generate-exponents production-inputs-count 1 production-inputs)
-          labor-exponents (generate-exponents production-inputs-count 2 production-inputs)
-          pollutant-exponents (generate-exponents production-inputs-count 3 production-inputs)]
-      (merge wc {:production-inputs production-inputs
-                 :effort-elasticity (rand-range 0.05 0.1) ; previously c
-                 :intermediate-input-exponents intermediate-input-exponents
-                 :nature-exponents nature-exponents
-                 :labor-exponents labor-exponents
-                 :pollutant-exponents pollutant-exponents
-                 :disutility-of-effort-exponent (rand-range 3 4) ; previously du
-                 :disutility-of-effort-coefficient 1 ; previously s
-                 :total-factor-productivity (rand-range 4 6) ; previously a
-                 :effort 0.5
-                 :output 0
-                 :labor-quantities [0]}))))
-
-; TODO : update input here?
 (defn create-wcs-bulk [num-ind-0 num-ind-1 num-ind-2]
   (let [input-cats (vec (range 1 11))]
     (->> (merge (create-wcs num-ind-0 input-cats 0)
                 (create-wcs num-ind-1 input-cats 1)
                 (create-wcs num-ind-2 input-cats 2))
          flatten
-         (mapv (partial continue-wc-setup
+         (mapv (partial gen-wc
                         input-cats ; intermediate-inputs
                         input-cats ; nature-types
                         input-cats ; labor-types
@@ -109,3 +70,5 @@
                         )))))
 
 ; (pequod-plus.wcs/create-wcs-bulk 100 100 100)
+
+; Why doesn't work for values < 10?
